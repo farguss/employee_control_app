c29c7b6d8e83caab57e2d41b6a639bd703fc6c51
farguss <andriyzhytskyi@gmail.com>
2019-05-12 13:32:51 +0300

    unit tests

---
+++ employee_UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("employee_UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("employee_UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1c042bc-21a3-4760-9100-a4646ce96ae6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ employee_UnitTestProject/TestSheduleService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using employee_control_BusinessLogicLayer.DTO;
+using employee_control_BusinessLogicLayer.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace employee_UnitTestProject
+{
+    [TestClass]
+    public class TestSheduleService
+    {
+
+        static string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=enterprise;";
+        ScheduleService schedule_service = new ScheduleService(connectionString);
+
+        [TestMethod]
+        public void TestGetWorker()
+        {
+            //Arrange
+
+            WorkerDTO new_worker = new WorkerDTO();
+
+            //Act
+
+            new_worker = schedule_service.GetWorker(100001);
+
+            //Assert
+
+            Assert.IsNotNull(new_worker);
+        }
+
+        [TestMethod]
+        public void TestGetWorkers()
+        {
+            //Arrange
+
+            List<WorkerDTO> list_of_workers = new List<WorkerDTO>();
+
+            //Act
+
+            list_of_workers = schedule_service.GetWorkers();
+
+            //Assert
+
+            Assert.IsNotNull(list_of_workers);
+        }
+
+        [TestMethod]
+        public void TestAddShedule()
+        {
+            //Arrange
+            ScheduleDTO new_schedule = new ScheduleDTO()
+            {
+                worker_id = 100001,
+
+                Monday = "10:00-15:00",
+
+                Tuesday = "10:00-18:00",
+
+                Wednesday = "10:00-20:00",
+
+                Thursday = "10:00-20:00",
+
+                Friday = "12:00-16:00",
+
+                Saturday = "holiday",
+
+                Sunday = "holiday",
+
+            };
+
+            Exception exception = null;
+
+
+            try
+            {
+                //Act
+                schedule_service.AddSchedule(new_schedule);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            //Assert
+            Assert.IsNull(exception);
+
+
+        }
+
+    }
+}
